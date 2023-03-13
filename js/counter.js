5dcfa53c078a1c719541f312fb71e252d90b7416
AleksandrLozovoy <lozovoialeksandr2019@gmail.com>
2023-03-13 20:52:40 +0300

    added counter in cards

---
+++ js/counter.js
@@ -0,0 +1,30 @@
+// const btnMinus = document.querySelector('[data-action="minus"]');
+// const btnPlus = document.querySelector('[data-action="plus"]');
+// const counter = document.querySelector("[data-counter]");
+
+// btnMinus.addEventListener("click", () => {
+//   if (parseInt(counter.innerText) > 1) counter.innerText = --counter.innerText;
+// });
+
+// btnPlus.addEventListener("click", () => {
+//   counter.innerText = ++counter.innerText;
+// });
+
+window.addEventListener("click", (event) => {
+  let counter;
+  if (
+    event.target.dataset.action === "minus" ||
+    event.target.dataset.action === "plus"
+  ) {
+    const counterWrapper = event.target.closest(".counter-wrapper");
+    counter = counterWrapper.querySelector("[data-counter]");
+  }
+  if (event.target.dataset.action === "minus") {
+    if (parseInt(counter.innerText) > 1)
+      counter.innerText = --counter.innerText;
+  }
+
+  if (event.target.dataset.action === "plus") {
+    counter.innerText = ++counter.innerText;
+  }
+});
